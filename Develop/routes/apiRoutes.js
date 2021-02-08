3b967581a7eb5b396944efe731f14ec1f5536c28
ala decoste <alshareefialaa@gmail.com>
2021-02-07 22:50:29 -0500

    Created POST, GET, and PUT APIs

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,75 @@
-var db = require("../models");
+const mongoose = require("mongoose");
+const db = require("../models");
 
 module.exports = function(app) {
- 
+
+    //GET last workout
+    app.get("/api/workouts/", (req, res)=>{
+        const id = req.params.id;
+        db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {$sum : "$exercises.duration"}
+                }
+            },
+        ]
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    //Add exercise
+    app.put("/api/workouts/:id", (req, res) => {
+        workouts
+          .findOneAndUpdate(
+            { _id: req.params.id },
+            {
+              $push: {
+                exercises: [req.body],
+              },
+            }
+          )
+          .then((newWorkout) => {
+            res.json(newWorkout);
+          })
+          .catch((err) => {
+            res.json(err);
+          });
+      });
+
+    //POST a new workout  
+    app.post("/api/workouts", (req, res) => {
+        console.log(req.params)
+        db.Workout.create(req.params)
+        .then(newWorkout => {
+            res.json(newWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+    });
+
+    //GET  duration of workout
+    app.get("/api/workouts/range", (req, res)=>{
+        db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {$sum : "$exercises.duration"}
+                }
+            },
+        ])
+        .sort({_id:-1})
+        .limit(7)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
 };
